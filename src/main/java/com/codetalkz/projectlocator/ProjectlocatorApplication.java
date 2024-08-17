9524389428653ffe48f251f05c1fc1cea274b284
Sofyan Pujas <gaero38@gmail.com>
2024-08-17 21:56:51 +0800

    Initial commit

---
+++ src/main/java/com/codetalkz/projectlocator/ProjectlocatorApplication.java
@@ -0,0 +1,13 @@
+package com.codetalkz.projectlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectlocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetalkz/projectlocator/ProjectlocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetalkz.projectlocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
