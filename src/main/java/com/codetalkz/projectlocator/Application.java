8baf727440cb645e84d45ee8a7c8eb055738e72b
Sofyan Pujas <gaero38@gmail.com>
2024-08-18 01:53:24 +0800

    refactor: remove unused implementations

--- src/main/java/com/codetalkz/projectlocator/Application.java
+++ src/main/java/com/codetalkz/projectlocator/Application.java
@@ -1,27 +1,13 @@
 package com.codetalkz.projectlocator;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.codetalkz.projectlocator.api.repositories.ProjectRepository;
-
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-	private final ProjectRepository projectRepository;
-
-	public Application(ProjectRepository projectRepository) {
-		this.projectRepository = projectRepository;
-	}
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		projectRepository.findAll().forEach(System.out::println);
 	}
 
 }
